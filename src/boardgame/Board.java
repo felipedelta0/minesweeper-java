93039eca174c884d5497eac1a8f3b232ecfbde52
Luis Felipe <felipedelta0@gmail.com>
2021-04-09 22:56:56 -0300

    Created Board and Tile files

---
+++ src/boardgame/Board.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+/**
+ * @author luisf on 09/04/2021
+ */
+public class Board {
+
+    private int rows;
+    private int columns;
+
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    private void initialSetup() {
+
+    }
+}
---
+++ src/boardgame/Tile.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+/**
+ * @author luisf on 09/04/2021
+ */
+public class Tile {
+
+    
+}
