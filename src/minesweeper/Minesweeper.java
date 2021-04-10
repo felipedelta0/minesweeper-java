f46e35311a28ecb78ebdf5a854f5a5271e5f00ab
Luis Felipe <felipedelta0@gmail.com>
2021-04-09 22:52:54 -0300

    Create Minesweeper.java

---
+++ src/minesweeper/Minesweeper.java
@@ -0,0 +1,12 @@
+package minesweeper;
+
+/**
+ * @author luisf on 09/04/2021
+ */
+public class Minesweeper {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
